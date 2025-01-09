c5f8b10013639fd0d4fdc1ebe940b9ac472be5ba
sagesteppe <reedbenkendorf27@gmail.com>
2025-01-09 17:02:34 -0600

    going after the og repo with mosaica in it, hopefully ignored

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A few scripts for creating bookmarks for giving out at conferences. In each case the goal of the bookmarks are to drive 'clicks' to websites. Inspired by conversations in Northwestern Universities PBC 450 seminar on Visual Presentations.
+
+The visual inspiration for this is work by Danielle Navarro which she shared at posticonf 2022, and is available to [view here](https://art-from-code.netlify.app/day-2/session-2/), and in a github repo [here](https://github.com/rstudio-conf-2022/art-from-code/tree/main). In particular her work on mosaica is the most visual I've ever seen some produce via code. 
--- scripts/Mosaics.R
+++ scripts/Mosaics.R
@@ -5,7 +5,10 @@ library(ggplot2)
 outer_border <- data.frame(
   x = c(0, 2, 2, 0, 0), 
   y = c(0, 0, 6, 6, 0)
-) |> sf::st_as_sf(coords = c('x', 'y'))
+) |> sf::st_as_sf(coords = c('x', 'y')) |>
+  dplyr::summarise(geometry = sf::st_combine(geometry)) %>%
+  sf::st_cast("POLYGON") |>
+  sf::st_as_sf()
 
 # now an inner border, 2 mm of width. this will be outside of our art, 
 # adjacent to the outer border. 
@@ -17,8 +20,6 @@ inner_border <- data.frame(
 ) |>  sf::st_as_sf(coords = c('x', 'y')) 
 
 
-y_pan <- c(0.2, 0.2, 1.6, 1.6, 0.2)
-
 #' Create consecutive panels within a regular shape 
 #' 
 #' @description Create consecutive panels in either the x or y dimension. 
@@ -29,6 +30,10 @@ y_pan <- c(0.2, 0.2, 1.6, 1.6, 0.2)
 #' @param add Numeric. The amount of area which needs to be added to have the panels border (see example).
 #' @results a list of panels, as sf polygons, for extracting as you see fit. 
 #' @examples 
+#' 
+#' # first define the corners of the shape you are going to panel. NOte that the
+#' # coordinates must go in ONE DIRECTION around the object, clockwise or counter
+#' # clockwise should not matter (to you). 
 #' x <- c(0.2, 1.8, 1.8, 0.2, 0.2) 
 #' y <- c(0.2, 0.2, 1.6, 1.6, 0.2)
 #' 
@@ -37,42 +42,96 @@ y_pan <- c(0.2, 0.2, 1.6, 1.6, 0.2)
 #' x = x, y = y
 #'  ) |>  sf::st_as_sf(coords = c('x', 'y')) |> 
 #'   ggplot2::ggplot() + ggplot2::geom_sf()
+#'   
+#' # note that generally... the amount of `add` will be the difference between the
+#' # two edges of your quadrilateral (yes this code will work for non-square -
+#' # but I am a square and so is my work). In this example, we are going to 
+#' # vertical, and our add is 1.4 (x[2] - x[1]). However we don't use this as a default
+#' # because there are a few different ways folks can supply these values. 
+#' 
+#' panels <- panelR(x, y, xORy= 'Y', n = 4, add = 1.4)
+#' ggplot2::ggplot(panels) + 
+#'   ggplot2::geom_sf()
 #' 
 #' @export 
 panelR <- function(x, y, xORy, n, add){
   
-  if(xORy=='x'){addX <- add; addY <- 1} else {addY <- add; add <- 1} 
+  if(xORy=='X'){addX <- add; addY <- 0} else {addY <- add; addX <- 0} 
   
+  mkPanel <- function(x, y, addX, addY){
+    data.frame(x = x + addX, y = y + addY) |>  
+      sf::st_as_sf(coords = c('x', 'y')) |>
+      dplyr::summarise(geometry = sf::st_combine(geometry)) %>%
+      sf::st_cast("POLYGON") |>
+      sf::st_as_sf()
+  }
   
+  panels <- vector(mode = 'list', length = n)
+  panels[1] <- mkPanel(x, y, addX = 0, addY = 0)
+  
+  for (i in 2:n){
+    panels[i] <- mkPanel(x, y, addX = addX*(i-1), addY = addY*(i-1))
+  }
+  
+  panels <- sf::st_combine(do.call("c", panels)) |>
+    sf::st_cast('POLYGON') |>
+    sf::st_as_sf() |>
+    dplyr::mutate(
+      Panel = 1:dplyr::n(), .before = 1) |>
+    dplyr::rename(geometry = x)
+  
+  return(panels)
 }
 
-data.frame(
-  x = x, y = y
-) |>  sf::st_as_sf(coords = c('x', 'y')) |> 
-  ggplot2::ggplot() + ggplot2::geom_sf()
 
-panel2 <- data.frame(
-  x = x, y = y_pan+1.4
-) |>  sf::st_as_sf(coords = c('x', 'y')) 
 
-panel3 <- data.frame(
-  x = x, y = y_pan+2.8
-) |>  sf::st_as_sf(coords = c('x', 'y')) 
+panels <- panelR(x, y, xORy= 'Y', n = 4, add = 1.4)
+ggplot2::ggplot(panels) + 
+  ggplot2::geom_sf()
 
-panel4 <- data.frame(
-  x = x, y = y_pan+4.2
-) |>  sf::st_as_sf(coords = c('x', 'y')) |>
-  dplyr::summarise(geometry = sf::st_combine(geometry)) %>%
-  st_cast("POLYGON") 
+# now we want to add grids into each of these panels. our printing goal will be
+# about 1.8 inches wide for each section and 1.6 long... We want same spaced grids. 
+# so this looks about right
+1.8/23
+1.6/20
+
+
+# create all mosaics for all panels at once, and then assign
+# creating by panel will result in some ugly clipping of them. 
+
+gr <- sf::st_make_grid(panels, n = c(20, 90), square = FALSE)
+gr <- sf::st_intersection(panels, gr)
+gr <- gr[ sf::st_is(gr, 'POLYGON'), ]
+
+
+??billow
+fractals <- list(billow, fbm, ridged)
+generators <- list(gen_simplex, gen_perlin, gen_worley)
+
+??fbm
+
+gr |>
+  dplyr::mutate(
+    fill = 10 * value + fracture(
+      x = x * sample(-3:3, 1),
+      y = y * sample(-3:3, 1),
+      noise = sample(generators, 1)[[1]],
+      fractal = sample(fractals, 1)[[1]],
+      octaves = sample(10, 1),
+      frequency = sample(10, 1) / 20,
+      value = "distance2"
+    ) |>
+      normalise(to = c(1, nshades)) |> 
+      round()
+  )
+
 
 ggplot() + 
-  geom_sf(data = outer_border) + 
-  geom_sf(data = inner_border) + 
-  geom_sf(data = panel1) + 
-  geom_sf(data = panel2) + 
-  geom_sf(data = panel3) + 
-  geom_sf(data = panel4)
+  geom_sf(data = outer_border, fill = '#BDC4A7') + 
+  geom_sf(data = inner_border, fill = NA) + 
+  geom_sf(data = panels, fill = NA) + 
+ geom_sf(data = gr)
 #  theme_void()
 
 
-
+?geom_sf()
